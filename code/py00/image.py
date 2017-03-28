760951352abcd5e3ab199217ae569eb8a6604abe
XrLee <715169084@qq.com>
2017-03-28 17:41:22 +0800

    the first python code, to add a number on the right top of a picture

--- code/py00/image.py
+++ code/py00/image.py
@@ -1,1 +1,14 @@
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
+
+def add_num(img):
+	draw = ImageDraw.Draw(img)
+	myfont = ImageFont.truetype('arial.ttf', size = 40)
+	fillcolor = '#ff0000'
+	width, height = img.size
+	draw.text((width - 40 * 2, 0), '99', font = myfont, fill = fillcolor)
+	img.save('result.jpg', 'jpeg')
+	return 0
+
+if __name__ == '__main__':
+	image = Image.open('huaji.jpg')
+	add_num(image)
