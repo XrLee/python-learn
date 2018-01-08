204d5db56c988e7c83621d698abdf767c0e4f6ef
XrLee <715169084@qq.com>
2018-01-08 13:34:37 +0800

    function unicode to help solve probelems with Chinese characters

--- code/py00/image.py
+++ code/py00/image.py
@@ -1,14 +1,24 @@
+# coding:utf-8
+"""this script add some text to a picture"""
+
 from PIL import Image, ImageDraw, ImageFont
 
 def add_num(img):
-	draw = ImageDraw.Draw(img)
-	myfont = ImageFont.truetype('arial.ttf', size = 40)
-	fillcolor = '#ff0000'
-	width, height = img.size
-	draw.text((width - 40 * 2, 0), '99', font = myfont, fill = fillcolor)
-	img.save('result.jpg', 'jpeg')
-	return 0
+    """add text to a picture
+
+    Args:
+    img: a picture
+
+    """
+    draw = ImageDraw.Draw(img)
+    myfont = ImageFont.truetype('C:/windows/fonts/simhei.ttf', size=40)
+    fillcolor = "#ff0000"
+    width, height = img.size
+    draw.text((width - 40 * 6, 0), unicode('还好有时光机', 'utf-8'), font=myfont, fill=fillcolor)
+    img.save('result.jpg', 'jpeg')
+    img.show()
+    return 0
 
 if __name__ == '__main__':
-	image = Image.open('huaji.jpg')
-	add_num(image)
+    IMAGE = Image.open('huaji.jpg')
+    add_num(IMAGE)
