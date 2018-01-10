69b46cc7c837320d583f028d5d9a436116e24b47
XrLee <715169084@qq.com>
2018-01-10 12:15:36 +0800

    improve the style of coding

--- code/py01/activation_code.py
+++ code/py01/activation_code.py
@@ -1,22 +1,31 @@
+""" this is a function to generate activation code"""
+
 import random
 import string
 
-def activation_code(id, length = 20):
-	'''id + "L" + random code'''
-	prefix = hex(int(id))[2:] + 'L'
-	length = length - len(prefix)
-	chars = string.ascii_letters + string.digits
-	'''
-	return prefix + ''.join([random.choice(chars) for i in range(length)])
-	'''
-	return prefix + ''.join(random.sample(chars,length))
+def activation_code(user_id, length=20):
+    """the form of activation code is id + "L" + random code
+
+    Args:
+    user_id: string, the id of user
+    length: the length of the activation code
+
+    """
+    prefix = hex(int(user_id))[2:] + 'L'
+    length = length - len(prefix)
+    chars = string.ascii_letters + string.digits
+
+    '''
+    return prefix + ''.join([random.choice(chars) for i in range(length)])
+    '''
+    return prefix + ''.join(random.sample(chars, length))
 
 def get_id(code):
-	'''hex to demical string'''
-	return str(int(code, 16))
+    '''hex to demical string'''
+    return str(int(code, 16))
 
 if __name__ == '__main__':
-	for i in range(1, 100, 10):
-		code = activation_code(i)
-		id = get_id(code.split('L')[0])
-		print code,id
+    for i in range(1, 100, 10):
+        CODE = activation_code(i)
+        ID = get_id(CODE.split('L')[0])
+        print CODE, ID
